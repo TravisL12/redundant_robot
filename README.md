e918c0ad1ae999cb81358f91fad5a28e207b0ea3
Travis Lawrence <travis.lawrence12@gmail.com>
2016-07-24 20:55:37 -0700

    Initial commit

--- README.md
+++
@@ -1,9 +0,0 @@
-## Building a shorthand guitar tab writer. This time with AngularJS.
-<br>
-### To do still:
-1. Add Lyrics input line
-1. Add Song Title Line
-1. Add Author line
-1. Clean Line-breaks with guitar strings reference
-1. Output to Text file
-1. Edit Chords/Tabs submitted
--- public/js/application.js
+++ public/js/application.js
@@ -1,124 +1,54 @@
-var guitarApp = angular.module('guitarApp', []);
+var squareGrid = document.getElementById('square-grid'),
+    width      = window.innerWidth+200,
+    height     = window.innerHeight+200,
+    cellDimension = 35,
+    rowCount   = Math.floor(height / cellDimension),
+    colCount   = Math.floor(width  / cellDimension),
+    animations = ['rotate-30',
+                  'rotate-40',
+                  'rotate-50',
+                  'rotate-60',
+                  'rotate-70',
+                  'rotate-80',
+                  'rotate-90'],
+    intervalDelay = 3000;
 
-guitarApp.controller('ChordCtrl', ['$scope', 'ChordLibrary', '$filter', function($scope, ChordLibrary, $filter){
+function randomChoice (collection) {
+  var index = Math.floor(Math.random() * collection.length);
+  return collection[index];
+}
 
-  $scope.spacer = "-"
-  $scope.chordLib = ChordLibrary;
-  $scope.tabs = [];
-  $scope.edit = false;
-
-  function blankChord(breaks) {
-    var breakCt = breaks || 4;
-    return {
-      breakCt: breaks,
-      chordInput: '',
-      stringInput: Array(6)
-    }
-  };
-
-  function titleCase(text) {
-    if (text) {
-      return text[0].toUpperCase() + text.slice(1);
-    }
+for (var i = 1; i < colCount; i++) {
+  if (lists !== undefined) {
+    lists += "<div class='column'><div class='inner-col'></div></div>";
+  } else {
+    var lists = "<div class='column'><div class='inner-col'></div></div>";
   }
+}
 
-  $scope.reset = function(breaks) {
-    $scope.inputForm = blankChord(breaks);
+for (var i = 1; i < rowCount; i++) {
+  if (columns !== undefined) {
+    columns += "<div class='row'>" + lists + "</div>";
+  } else {
+    var columns = "<div class='row'>" + lists + "</div>";
   }
-  $scope.reset(4);
+}
 
-  $scope.resetAll = function() {
-    $scope.tabs = [];
-    $scope.reset(4);
-  }
+squareGrid.innerHTML = columns;
+var rows = squareGrid.children;
 
-  $scope.buildRawTab = function() {
-    var strings = $scope.inputForm.stringNumbers.split('');
-    var frets = $scope.inputForm.fretNumbers.split('');
+function timeRows () {
+  var row = randomChoice(rows);
+  var animationClass = randomChoice(animations);
+  timeCols(row, animationClass);
+}
 
-    for(var i=0; i<strings.length; i++) {
-      $scope.inputForm.stringInput[strings[i] - 1] = frets[i];
-    }
-    $scope.addTab();
-  }
+function timeCols (row, animation) {
+  var column = randomChoice(row.children);
+  column.children[0].classList.add(animation);
+}
 
-  $scope.printSpacing = function(spaces, character) {
-    var joinVal = character || $scope.spacer;
-    return Array(spaces + 1).join(joinVal);
-  }
-
-  $scope.insertTab = function() {
-    $scope.editIdx = this.$index;
-    $scope.tabs.splice($scope.editIdx, 0, blankChord($scope.inputForm.breakCt));
-    $scope.edit = true;
-  }
-
-  $scope.editTab = function() {
-    $scope.inputForm.chordInput = $scope.tabs[this.$index].chordInput;
-    $scope.inputForm.stringInput = $scope.tabs[this.$index].stringInput;
-    $scope.editIdx = this.$index;
-    $scope.edit = true;
-  }
-
-  $scope.saveEditTab = function() {
-    $scope.tabs[$scope.editIdx].chordInput  = $scope.inputForm.chordInput;
-    $scope.tabs[$scope.editIdx].stringInput = $scope.inputForm.stringInput;
-    $scope.reset($scope.inputForm.breakCt);
-    $scope.edit = false;
-  }
-
-  $scope.deleteTab = function() {
-    var idx = this.$index;
-    $scope.tabs.splice(idx, 1);
-  }
-
-  $scope.editMeasure = function(num) {
-    if (num > 0) {
-      $scope.tabs[this.$index].breakCt += 1
-    } else if ($scope.tabs[this.$index].breakCt > 0) {
-      $scope.tabs[this.$index].breakCt -= 1;
-    }
-  }
-
-  $scope.lookupChord = function(){
-    $scope.inputForm.chordInput = titleCase($scope.inputForm.chordInput);
-    for(var i=0;i<ChordLibrary.length; i++){
-      if($scope.inputForm.chordInput === ChordLibrary[i].name){
-        $scope.inputForm.stringInput = ChordLibrary[i].tab;
-      }
-    }
-  }
-
-  $scope.addTab = function() {
-    if ($scope.inputForm.stringInput.length > 0 && !$scope.edit) {
-      $scope.tabs.push($scope.inputForm);
-      $scope.reset($scope.inputForm.breakCt);
-      document.getElementById("chord-input").focus();
-    } else{
-      console.log('Empty Forms');
-    }
-  }
-
-  $scope.addMeasure = function() {
-    $scope.inputForm.chordInput = '|';
-    $scope.lookupChord();
-    $scope.tabs.push($scope.inputForm);
-    $scope.reset($scope.inputForm.breakCt);
-  }
-
-  $scope.clickedFilteredChord = function(choice) {
-    $scope.inputForm.chordInput = choice.name;
-    $scope.inputForm.stringInput = choice.tab;
-    $scope.edit ? $scope.saveEditTab() : $scope.addTab();
-  }
-
-  $scope.pressEnter = function(e) {
-    if($scope.inputForm.chordInput || e.which == 77){
-      if(e.which==13){
-        $scope.edit ? $scope.saveEditTab() : $scope.addTab();
-      };
-      if(e.which==77){ $scope.addMeasure() };
-    }
-  }
-
-}])
+timeRows();
+setInterval(function () {
+  timeRows();
+}, intervalDelay);
