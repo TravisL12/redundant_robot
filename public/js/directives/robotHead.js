e29562e99dfbe9fb14a073c640a281bdb92212a6
Travis Lawrence <tlawrence@tumblr.com>
2017-11-14 17:42:46 -0800

    Remove public folder

--- public/js/directives/robotHead.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('redundantRobot').directive('robotHead', function () {
-  return {
-    restrict: 'E',
-    replace: true,
-    templateUrl: 'public/views/robot_head.html'
-  }
-})
