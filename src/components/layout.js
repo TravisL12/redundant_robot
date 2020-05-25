e519fcb9962efb5b7f1f209c62cc841b3378651e
Travis Lawrence <travis.lawrence12@gmail.com>
2020-05-24 18:20:56 -0700

    BG color fade

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,6 +6,14 @@ import Header from "./header"
 import "../styles/application.scss"
 import { RobotHead } from "./robot-head"
 
+// Slowly change background color of site
+const bgColors = ["#C0D8E0", "#C7CCE5", "#FFF4D9", "#FFEDD9"]
+let colorIdx = bgColors.length
+setInterval(() => {
+  document.body.style.backgroundColor = bgColors[colorIdx % bgColors.length]
+  colorIdx++
+}, 8000)
+
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
