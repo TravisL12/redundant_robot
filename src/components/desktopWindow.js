fef4cd10bf8c34fdabd5f8d218f6a784299e2277
Travis Lawrence <travis.lawrence12@gmail.com>
2020-07-25 18:34:40 -0700

    Just use a library

--- src/components/desktopWindow.js
+++ src/components/desktopWindow.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Draggable from "react-draggable";
 
 const DesktopWindow = () => {
   return (
-    <div className="window">
-      <div className="window-title-bar">My Files</div>
-      <div className="window-body"></div>
-    </div>
+    <Draggable handle=".window-title-bar">
+      <div className="window">
+        <div className="window-title-bar">My Files</div>
+        <div className="window-body"></div>
+      </div>
+    </Draggable>
   );
 };
 
