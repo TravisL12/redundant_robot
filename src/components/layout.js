e5076fdaafc1e7027a942197aa07b0141c44a2f3
Travis Lawrence <travis.lawrence12@gmail.com>
2020-07-28 17:32:47 -0700

    FIgure out new window IDs

--- src/components/layout.js
+++ src/components/layout.js
@@ -21,12 +21,31 @@ const defaultWindows = [
   { title: "4 Demos" },
 ];
 
+function findWindowId(windows, nextId) {
+  const ids = windows.map(({ id }) => id).sort();
+  if (!ids.includes(nextId)) {
+    return nextId;
+  }
+  for (let i = 0; i < ids.length; i++) {
+    if (!ids.includes(ids[i] + 1)) {
+      return ids[i] + 1;
+    }
+  }
+  return ids[ids.length - 1] + 1;
+}
+
 function orderWindows(windows) {
-  return windows.map((window, idx) => ({
-    ...window,
-    id: window.id || idx,
-    sort: window.sort || idx,
-  }));
+  return windows.reduce((acc, window, idx) => {
+    const newId = window.hasOwnProperty("id")
+      ? window.id
+      : findWindowId([...windows, ...acc], idx);
+    acc.push({
+      ...window,
+      id: newId,
+      sort: window.sort || idx,
+    });
+    return acc;
+  }, []);
 }
 
 const Layout = () => {
