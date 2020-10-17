f800f5e72f9087129f3bf8b22a0c682d1a2aed4e
Travis Lawrence <travis.lawrence12@gmail.com>
2020-10-17 14:30:06 -0700

    Remove log

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,23 +4,20 @@ import { Link } from 'react-router-dom';
 import portfolioData from './portfolioData.json';
 import PortfolioSite from './PortfolioSite';
 
-function App() {
-  console.log(portfolioData);
-  return (
-    <div className="home-content">
-      <p>
-        Redundant Robot is a little site I built that, for several years, has
-        mainly been home to my SheepShaver tutorial. Since becoming a{' '}
-        <Link to="/resume">web developer</Link> I like to hack away on some of
-        my ideas and post them here for people to check out. Feel free to give a
-        shout out on <a href="https://twitter.com/travisl12">twitter</a>.
-      </p>
+const App = () => (
+  <div className="home-content">
+    <p>
+      Redundant Robot is a little site I built that, for several years, has
+      mainly been home to my SheepShaver tutorial. Since becoming a{' '}
+      <Link to="/resume">web developer</Link> I like to hack away on some of my
+      ideas and post them here for people to check out. Feel free to give a
+      shout out on <a href="https://twitter.com/travisl12">twitter</a>.
+    </p>
 
-      {portfolioData.map((data) => {
-        return <PortfolioSite {...data} />;
-      })}
-    </div>
-  );
-}
+    {portfolioData.map((data) => {
+      return <PortfolioSite {...data} />;
+    })}
+  </div>
+);
 
 export default App;
