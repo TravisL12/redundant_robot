3dca367ef268060893557aa237cfe9e8b77e7ce0
Travis Lawrence <travis.lawrence12@gmail.com>
2021-10-19 09:40:00 -0700

    fix linked in href

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,45 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { HeaderContainer } from '../../styles/header';
-import { RobotCube } from '../RobotHead';
-import { getAsset } from '../utilities';
+import React from "react";
+import { Link } from "react-router-dom";
+import { HeaderContainer } from "../../styles/header";
+import { RobotCube } from "../RobotHead";
+import { getAsset } from "../utilities";
 
 const GithubSocialMediaLogo = getAsset(
-  '/assets/social/iconfinder_github_317712.svg'
+  "/assets/social/iconfinder_github_317712.svg"
 );
 const OnlineSocialMediaTwitter = getAsset(
-  '/assets/social/iconfinder_twitter_317720.svg'
+  "/assets/social/iconfinder_twitter_317720.svg"
 );
 const OnlineSocialMediaLinkedIn = getAsset(
-  '/assets/social/iconfinder_square-linkedin_317725.svg'
+  "/assets/social/iconfinder_square-linkedin_317725.svg"
 );
-const CodePenIcon = getAsset('/assets/social/codepen_logo.svg');
-const ResumeIcon = getAsset('/assets/social/resume_logo.svg');
+const CodePenIcon = getAsset("/assets/social/codepen_logo.svg");
+const ResumeIcon = getAsset("/assets/social/resume_logo.svg");
 
 const Header = () => {
   const socialLinks = [
     {
-      name: 'Github',
+      name: "Github",
       icon: GithubSocialMediaLogo,
-      url: 'https://github.com/travisl12',
+      url: "https://github.com/travisl12",
     },
     {
-      name: 'Twitter',
+      name: "Twitter",
       icon: OnlineSocialMediaTwitter,
-      url: 'https://www.twitter.com/travisl12',
+      url: "https://www.twitter.com/travisl12",
     },
     {
-      name: 'LinkedIn',
+      name: "LinkedIn",
       icon: OnlineSocialMediaLinkedIn,
-      url: 'https://www.linkedin.com/in/travis-lawrence-b77400b8',
+      url: "https://www.linkedin.com/in/travis-l-b77400b8/",
     },
-    { name: 'Codepen', icon: CodePenIcon, url: 'https://codepen.io/travisl12' },
+    { name: "Codepen", icon: CodePenIcon, url: "https://codepen.io/travisl12" },
   ];
 
   return (
     <HeaderContainer>
       <div className="header-container">
-        <div style={{ display: 'flex' }}>
+        <div style={{ display: "flex" }}>
           <RobotCube />
           <div className="home-title">
             <Link to="/">
